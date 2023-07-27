1db108e3be9a60091c28bac4b906f0a1bf171a08
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2023-07-27 13:06:13 -0300

    aprendi operador ternario

--- learning-js/aula1/script.js
+++ learning-js/aula1/script.js
@@ -1,11 +1,5 @@
 var idade;
-var pedido;
-idade= parseInt(prompt("qual a sua idade?"));
 
-if(idade>=18){
-    pedido= prompt("qual o seu pedido?");
-    alert("o pedido de: " + pedido + " foi feito");
-}
-else {
-    alert("a venda e proibida para menores de idade");
-}
+idade=parseInt(prompt("qual a sua idade?"));
+
+idade>18 ? alert("pode prosseguir") : alert("saia imediatamente");
