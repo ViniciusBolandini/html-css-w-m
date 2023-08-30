bbf469cc6f28af134adcafa721bbaad3d2c96880
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2023-08-30 16:47:20 -0300

    coloquei a div com a imagem

--- learning-js/aula1/script.js
+++ learning-js/aula1/script.js
@@ -1,11 +1,8 @@
-var nota= new Array();
-var media;
 
-nota[0]=prompt("qual seu nome");
-
-console.log(nota[0]);
-
+function media(n1,n2){
+    var media = (n1+n2)/2;
+    return media
+}
 
 
-
-
+console.log(media(7,5));
