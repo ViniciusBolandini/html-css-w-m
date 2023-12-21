35f905d2be8e28db565d74bc86edcfd499923106
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2023-12-21 12:12:47 -0300

    estudei js

--- learning-js/aula1/script.js
+++ learning-js/aula1/script.js
@@ -1,7 +1,16 @@
-function abremenu(i){
-    i.innerHTML = "<p>op1</p>" + "<p>op2</p>"
-}
+var n = "vinicius";
+var s = "1235";
 
-function fechamenu(i){
-    i.innerHTML = "<p>=</p>" + "<style>.menu{background-color: aqua;height: 35px;}</style>"
+function loga(){
+    let nome = document.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+
+    if(nome == n && senha == s){
+        let para = document.getElementById("res");
+        para.innerText = "login success"
+    }
+    else{
+        let para = document.getElementById("res");
+        para.innerText = "login fail"
+    }
 }
