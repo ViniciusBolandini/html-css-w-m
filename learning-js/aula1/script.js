8d3e4da6eba9a04076b6cb405de44260775886d9
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2024-01-03 19:04:41 -0300

    fiz um exerciio de js

--- learning-js/aula1/script.js
+++ learning-js/aula1/script.js
@@ -1,14 +1,27 @@
-function somar(){
-    var n1;
-    var n2;
-    var res;
-    var resposta;
-    n1 = document.getElementById('n1').value;
-    n2 = document.getElementById('n2').value;
-    res = parseInt(n1) + parseInt(n2);
-    
-    
-    resposta = document.getElementById('resp');
-    resposta.innerText = res;
+var pegadata = new Date();
+var hora = pegadata.getHours();
+var minutos = pegadata.getMinutes();
 
+var texto_hora = document.getElementById('texto');
+texto_hora.innerText = `${hora}:${minutos}`;
+
+
+
+if(hora < 12 && hora >6){
+    let muda_foto = document.getElementById('imagem');
+    muda_foto.src = "imagens/manha.jpg";
+    let fundo = document.body;
+    fundo.style.background = '#6EAFC5';
+}
+if(hora > 12 && hora <= 18){
+    let muda_foto = document.getElementById('imagem');
+    muda_foto.src = "imagens/tarde.jpg";
+    let fundo = document.body;
+    fundo.style.background = '#F2A663';
+}
+if(hora > 18 && hora <23){
+    let muda_foto = document.getElementById('imagem');
+    muda_foto.src = "imagens/noite.jpg";
+    let fundo = document.body;
+    fundo.style.background = '#324252';
 }
