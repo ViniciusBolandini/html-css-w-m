81eb249d029129438bb8399e8a1b540f08dbe873
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2024-01-07 13:14:42 -0300

    fiz mais da pagina de noticias

--- learning-js/aula6/script.js
+++ learning-js/aula6/script.js
@@ -6,8 +6,40 @@ function abrirmenu(){
 }
 
 function escondemenu(){
+    let publicador = document.getElementById('publi');
+
+    if(publicador.style.display == 'none'){
+        let divmenu = document.getElementById('menu');
+        divmenu.style.display = 'none';
+        let asidemenu = document.getElementById('asidemenu');
+        asidemenu.style.display = 'none';
+    }
+}
+
+function abrepublicador(){
+    let publicador = document.getElementById('publi');
+    publicador.style.display = 'inline-block';
     let divmenu = document.getElementById('menu');
     divmenu.style.display = 'none';
+}
+
+function fechapublicador(){
+    let publicador = document.getElementById('publi');
+    publicador.style.display = 'none';
     let asidemenu = document.getElementById('asidemenu');
     asidemenu.style.display = 'none';
+}
+
+function verificasefecha(){
+    let inptitulo = document.getElementById('inptitulo');
+    let inpnoticia = document.getElementById('inpnoticia');
+    let resp = confirm('você abandonará essa publicação');
+    if(resp == true){
+        fechapublicador();
+    }
+}
+
+function publicar(){
+    let inptitulo = document.getElementById('inptitulo');
+    let inpnoticia = document.getElementById('inpnoticia');
 }
