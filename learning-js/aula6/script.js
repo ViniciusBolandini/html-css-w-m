7ba9960bed80ff2b5dd03f5ea89f4029fe90e434
ViniciusBolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2024-01-07 11:18:54 -0300

    fiz site de noticizx

---
+++ learning-js/aula6/script.js
@@ -0,0 +1,13 @@
+function abrirmenu(){
+    let divmenu = document.getElementById('menu');
+    divmenu.style.display = 'inline-block';
+    let asidemenu = document.getElementById('asidemenu');
+    asidemenu.style.display = 'inline-block';
+}
+
+function escondemenu(){
+    let divmenu = document.getElementById('menu');
+    divmenu.style.display = 'none';
+    let asidemenu = document.getElementById('asidemenu');
+    asidemenu.style.display = 'none';
+}
